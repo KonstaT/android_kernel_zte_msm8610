0acecf26a40bb93caeb92b5554ff26de7fdba0b9
savoca <adeddo27@gmail.com>
2014-11-20 01:46:55 -0500

    LCD_KCAL: Color control driver for msm8x10/mdp3
    Major thanks to @faux123 and LGE for previous implementations and reference.
    
    Change-Id: I1b9b55506000997aea3280b5787d722f9fe9b287

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -2612,7 +2612,11 @@ static int mdss_fb_set_lut(struct fb_info *info, void __user *p)
 	if (ret)
 		return ret;
 
+#ifdef CONFIG_LCD_KCAL
+	mfd->mdp.lut_update(mfd, &cmap, false);
+#else
 	mfd->mdp.lut_update(mfd, &cmap);
+#endif
 	return 0;
 }
 
